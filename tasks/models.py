00cf981fcd9eb212fc282cd3ec708bcbcc900b73
William Sandbrink <wilsandbrink@gmail.com>
2019-03-28 09:36:10 +0100

    started creating task model (never migrated)

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=450, required=True)
+    is_done = models.BooleanField(default=False)
