325a8cc805d90e77028c17d98a8bdacb545b24fe
William Sandbrink <wilsandbrink@gmail.com>
2019-04-05 11:20:18 +0200

    idk

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+# Todo: Subclass login view and add title
 
 def register(request):
     if request.method == 'POST':
@@ -14,7 +15,8 @@ def register(request):
         form = UserCreationForm()
 
     context = {
-        'form': form
+        'form': form,
+        'title': 'Register'
     }
     template = 'users/register.html'
 
@@ -26,7 +28,8 @@ def profile(request):
     user = request.user
 
     context = {
-        'user': user
+        'user': user,
+        'title': 'Profile'
     }
     template = 'users/profile.html'
 
