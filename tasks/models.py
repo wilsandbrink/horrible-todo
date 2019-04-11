dbc830e860bffc048b61a18561f1e77e19488de5
William Sandbrink <wilsandbrink@gmail.com>
2019-04-11 10:00:41 +0200

    Revert "started creating models for projects and organizations"
    This reverts commit 57a2027079ce479bc743e1452b03805abbac82cd.

--- tasks/models.py
+++ tasks/models.py
@@ -2,18 +2,6 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# Work in progress
-
-class Organization(models.Model):
-    name = models.CharField(max_length=450)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    members = models.ManyToManyField(User)
-
-
-class Project(models.Model):
-    owner = models.ForeignKey(Organization)
-    title = models.CharField(max_length=450)
-
 
 class Task(models.Model):
     title = models.CharField(max_length=450)
